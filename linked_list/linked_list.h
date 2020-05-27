4b7d440de89db59ee2e5bb80be36d86981f89812
Roberto Ribeiro <rober.will@hotmail.com>
2020-05-27 23:46:41 +0100

    new file:   linked_list/linked_list.c 	new file:   linked_list/linked_list.h 	new file:   linked_list/main.c Hello, commit!

---
+++ linked_list/linked_list.h
@@ -0,0 +1,15 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FALSE    0
+#define TRUE     1
+#define nil      NULL
+
+typedef struct {
+    int number;
+} ListItem;
+
+#endif

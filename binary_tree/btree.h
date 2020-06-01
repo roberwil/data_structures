3ae1db6ea970361a9acacb1935bec6fdfedb1a7e
Roberto Ribeiro <rober.will@hotmail.com>
2020-06-01 23:00:48 +0100

    new file:   binary_tree/btree.c 	new file:   binary_tree/btree.h 	new file:   binary_tree/main.c Hello, commit!

---
+++ binary_tree/btree.h
@@ -0,0 +1,36 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FALSE 0
+#define TRUE  1
+#define nil   NULL
+#define and   &&
+#define or    ||
+
+typedef struct {
+    int number;
+} BTreeItem;
+
+typedef struct _node {
+    BTreeItem item;
+    struct _node* right;
+    struct _node* left;
+} _node;
+
+typedef struct  {
+    _node* root;
+} _btree;
+
+typedef _node* Node;
+typedef _btree* BTree;
+
+void init(BTree tree);
+Node create_root(BTree tree, BTreeItem* item);
+
+Node insert_right(Node node, BTreeItem* item);
+Node insert_left(Node node, BTreeItem* item);
+
+#endif 
